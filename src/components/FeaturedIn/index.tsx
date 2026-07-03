e9af5598a639bf83fc1818f751867b973e21c4aa
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-03 15:09:37 +0300

    tweaks

--- src/components/FeaturedIn/index.tsx
+++ src/components/FeaturedIn/index.tsx
@@ -2,12 +2,12 @@ import config from './config'
 
 export const FeaturedIn = () => {
   return (
-    <div className="col-[main] grid gap-10 my-10">
-      <h2 className="text-white text-5xl font-semibold">Featured In</h2>
+    <div className="col-[main] my-10 grid gap-10">
+      <h2 className="text-5xl font-semibold text-white">Featured In</h2>
 
-      <ul className="flex gap-4 justify-between">
+      <ul className="flex items-center justify-between gap-4">
         {config.featuredIn.map((item, i) => (
-          <li key={i}>
+          <li key={i} className="bg-darknavy/80 backdrop-blur-lg">
             <img src={item} alt="" />
           </li>
         ))}
