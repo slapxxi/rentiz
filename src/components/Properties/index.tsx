c400fa0c62b00d710ad30d992c9eda023834e83f
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-03 19:36:55 +0300

    responsive + tweaks

--- src/components/Properties/index.tsx
+++ src/components/Properties/index.tsx
@@ -1,39 +1,39 @@
+import AreaIcon from '~/assets/area.svg?react'
+import BathIcon from '~/assets/bath.svg?react'
+import BedIcon from '~/assets/bed.svg?react'
 import ChevronLeft from '~/assets/chevron-left.svg?react'
 import ChevronRight from '~/assets/chevron-right.svg?react'
-import patternImage from '~/assets/geo-pattern.png'
-import BedIcon from '~/assets/bed.svg?react'
-import BathIcon from '~/assets/bath.svg?react'
-import AreaIcon from '~/assets/area.svg?react'
 import config from './config'
 
 export const Properties = () => {
   return (
-    <div
-      className={`col-[wide] grid grid-cols-subgrid my-10 gap-y-12 bg-[url(${patternImage})]`}
-    >
+    <div className="geo-section col-[wide] my-10 grid grid-cols-subgrid gap-y-12">
       <div className="col-[main]">
         <h2 className="text-5xl font-semibold">Popular Residence</h2>
       </div>
 
-      <div className="col-[wide] grid grid-cols-[1fr_minmax(auto,1680px)_1fr] gap-4">
-        <div className="col-2 grid grid-cols-[auto_1fr_auto] items-center gap-12.5">
-          <button className="border border-white rounded-full size-20 inline-grid place-items-center">
+      <div className="col-[main] grid gap-4 md:col-[wide] md:grid-cols-[1fr_minmax(auto,1680px)_1fr]">
+        <div className="grid gap-12.5 md:col-2 md:row-span-4 md:grid-cols-[auto_1fr_auto] md:grid-rows-subgrid">
+          <button className="hidden size-20 place-items-center self-center rounded-full border border-white md:inline-grid">
             <ChevronLeft className="w-8" />
           </button>
 
-          <ul className="grid grid-cols-3 gap-8">
+          <ul className="grid gap-8 md:row-span-4 md:grid-cols-3 md:grid-rows-subgrid">
             {config.items.map((item, i) => (
-              <li key={i} className="grid gap-2">
+              <li
+                key={i}
+                className="grid gap-2 md:row-span-4 md:grid-rows-subgrid"
+              >
                 <img
                   src={item.img}
                   alt=""
-                  className="w-full aspect-square object-cover"
+                  className="aspect-square w-full object-cover"
                 />
-                <h3 className="font-semibold text-white text-2xl">
+                <h3 className="text-2xl font-semibold text-white">
                   {item.name}
                 </h3>
-                <h4 className="text-neutral-300 text-lg">{item.address}</h4>
-                <div className="flex gap-4 justify-between">
+                <h4 className="text-lg text-neutral-300">{item.address}</h4>
+                <div className="flex justify-between gap-4">
                   <div className="flex items-center gap-1">
                     <BedIcon className="text-teal-600" />
                     {item.beds} Beds
@@ -51,7 +51,7 @@ export const Properties = () => {
             ))}
           </ul>
 
-          <button className="border border-white rounded-full size-20 inline-grid place-items-center">
+          <button className="hidden size-20 place-items-center self-center rounded-full border border-white md:inline-grid">
             <ChevronRight className="w-8" />
           </button>
         </div>
